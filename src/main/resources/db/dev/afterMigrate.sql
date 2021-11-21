0142ebf755a0fb0a2fb3cf0a350937142dc58d52
Vinicius Rodrigues <vinicius.rodrigues.lima@ccc.ufcg.edu.br>
2021-11-21 13:22:55 -0300

    fix afterMigrate bug

--- src/main/resources/db/dev/afterMigrate.sql
+++ src/main/resources/db/dev/afterMigrate.sql
@@ -1,9 +1,13 @@
 delete from effort;
 delete from task;
+delete from user_roles;
+delete from auth_user;
 
-insert into task (id, done, resume, user_id) values (1001, false, 'configurar login do sistema', 1);
-insert into task (id, done, resume, user_id) values (1002, false, 'adicionar recurso restaurante', 1);
-insert into task (id, done, resume, user_id) values (1003, true, 'conserta bug na paginacao', 1);
+insert into auth_user (id, username, password) values (1001, 'vinicius', 'vinicius123');
+
+insert into task (id, done, resume, user_id) values (1001, false, 'configurar login do sistema', 1001);
+insert into task (id, done, resume, user_id) values (1002, false, 'adicionar recurso restaurante', 1001);
+insert into task (id, done, resume, user_id) values (1003, true, 'conserta bug na paginacao', 1001);
 
 insert into effort (id, initial, termination, task_id) values (1001, current_timestamp, null, 1001);
 insert into effort (id, initial, termination, task_id) values (1002, current_timestamp, current_timestamp, 1001);
